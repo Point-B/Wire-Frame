f8ba107d286f49a946223c0ee17f523c9fc61f99
brettmagnus <brettmagnus@me.com>
2015-02-23 17:22:11 -0500

    WireFrame

---
+++ PointB/DashViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DashViewController.swift
+//  PointB
+//
+//  Created by Brett Magnus on 2/19/15.
+//  Copyright (c) 2015 The Beards. All rights reserved.
+//
+
+import UIKit
+
+class DashViewController: UIViewController {
+    @IBOutlet weak var NameSU: UITextField!
+    @IBOutlet weak var emailSU: UITextField!
+    @IBOutlet weak var passwordSU: UITextField!
+    @IBOutlet weak var cpasswordSU: UITextField!
+    @IBOutlet weak var carmakeSU: UITextField!
+    @IBOutlet weak var carmodelSU: UITextField!
+    @IBOutlet weak var licenseSU: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ PointB/signinVC.swift
@@ -0,0 +1,35 @@
+//
+//  signinVC.swift
+//  PointB
+//
+//  Created by Brett Magnus on 2/23/15.
+//  Copyright (c) 2015 The Beards. All rights reserved.
+//
+
+import UIKit
+
+class signinVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
